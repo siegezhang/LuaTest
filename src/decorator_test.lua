92d7ba06987c14e42bd23fc98f78ad26029d455c
zhangxiaosheng <z1178858918@qq.com>
2022-11-05 18:27:51 +0800

    commit

--- src/decorator_test.lua
+++ src/decorator_test.lua
@@ -10,6 +10,15 @@
 --One solution is basically to create a global table that maps objects (as keys) to their annotations (as values). By definition, objects have unique identities and therefore can serve as unique keys in a table. In this way, the objects themselves are not modified. This interferes a bit with garbage collection since the global table holds references to the objects, but we can use a "weak table" in Lua for this purpose (see the book Programming in Lua in LuaBooks).
 --
 --Here's a way to apply docstrings [1] to Lua objects.
+--弱引用table有3种类型
+--1、具有弱引用key的table；
+--2、具有弱引用value的table；
+--3、同时具有弱引用key和value的table；
+--
+--table的弱引用类型是通过其元表中的__mode字段来决定的。
+--这个字段的值应为一个字符串：
+--如果包含’k’，那么这个table的key是弱引用的；
+--如果包含’v’，那么这个table的value是弱引用的；
 
 local docstrings = setmetatable({}, { __mode = "kv" })
 
@@ -42,7 +51,20 @@ help(f)
 --This pattern can be applied to other situations in which you wish to apply metadata to objects without making any change to those objects. These include function decorators as used in Python [2] [3].
 --
 --The following alternative syntax for applying function decorators to functions could be preferable:
+function typecheck(...)
+    return setmetatable({ ... }, mt)
+end
 
+function docstring(...)
+    return setmetatable({ ... }, mt)
+end
+mt = { __concat = function(a, f)
+    return function(...)
+        print("decorator", table.concat(a, ","), ...)
+        return f(...)
+    end
+end
+}
 random = docstring [[Compute random number.]] ..
         typecheck("number", '->', "number") ..
         function(n)
@@ -51,21 +73,8 @@ random = docstring [[Compute random number.]] ..
 
 --The function decorators can be implemented basically in this way:
 
-mt = { __concat = function(a, f)
-    return function(...)
-        print("decorator", table.concat(a, ","), ...)
-        return f(...)
-    end
-end
-}
 
-function typecheck(...)
-    return setmetatable({ ... }, mt)
-end
 
-function docstring(...)
-    return setmetatable({ ... }, mt)
-end
 
 --The use of an operator (..) here between the function decorator and the function avoids the parenthesis around the function and makes chaining decorators nicer. We want this operator to have right associativity, so the only choices for this operator are .. and ^.
 --
